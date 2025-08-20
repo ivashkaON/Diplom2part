c5fb0c0a539609a7c82776844da98bd913f34c2b
Иван Рыжих <ivashkaON@yandex.ru>
2025-08-20 09:35:09 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,7 @@
 # Иван Рыжих Инженер по тестированию плюс Дипломный проект 2 часть
-URL_SERVICE = "https://beaccf23-4da7-4b3d-a100-302463e59fa5.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://fd629e23-add7-4160-bd22-3b3a546fc4c5.serverhub.praktikum-services.ru"
 
 CREATE_ORDER = "/api/v1/orders"
+
 
 GET_TRACK = "/api/v1/orders/track"
