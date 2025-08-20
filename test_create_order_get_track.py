b5a8d67f30e3d18561c50b7253e63fd2820af03a
Иван Рыжих <ivashkaON@yandex.ru>
2025-08-20 21:24:41 +0300

    Update test_create_order_get_track.py

--- test_create_order_get_track.py
+++ test_create_order_get_track.py
@@ -7,12 +7,12 @@ def test_track_order():
 
 
     create_json = create_resp.json()
-
     track = create_json["track"]
 
    
     track_resp = api.get_track_order(track)
     assert track_resp.status_code == 200
-
+#Привет, Екатерина. Спасибо за пояснение, не сразу понял, что именно требуется. Надеюсь, теперь всё в порядке
 
     
+
