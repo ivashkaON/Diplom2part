6bf8836926b30517e5fd40bed59e7ef7295a6fe4
Иван Рыжих <ivashkaON@yandex.ru>
2025-08-20 21:20:06 +0300

    Update test_create_order_get_track.py

--- test_create_order_get_track.py
+++ test_create_order_get_track.py
@@ -4,14 +4,15 @@ import request_data as data
 def test_track_order():
     
     create_resp = api.post_new_order(data.order_body)
-    assert create_resp.status_code in (200, 201)
+
 
     create_json = create_resp.json()
-    assert "track" in create_json
+
     track = create_json["track"]
 
    
     track_resp = api.get_track_order(track)
     assert track_resp.status_code == 200
+
 
     
